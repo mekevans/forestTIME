e1f4699b96b14275869c4b42d4a88f8f510c1b53
Renata Diaz <renatadiaz@arizona.edu>
2024-01-09 13:52:29 -0700

    Sets up to filter on PLOT UNIQUE ID. Next I am going to add cols from PLOT, CONDITION tables.

--- R/demo_functions_as_fs.R
+++ R/demo_functions_as_fs.R
@@ -5,6 +5,7 @@ select_trees <- function(state = 27,
                          county = 0:1000,
                          plot = 0:100000,
                          spcd = 0:10000,
+                         plot_unique_id = NULL,
                          min_years = 1,
                          min_measurements = 1,
                          connection = "local",
@@ -34,9 +35,17 @@ select_trees <- function(state = 27,
       NYEARS_MEASURED >= min_measurements,
       LASTYR - FIRSTYR >= min_years
     ) |>
-    collect()
+    compute()
   
-  tree_info
+  if(!is.null(plot_unique_id)) {
+    tree_info <- tree_info |>
+      filter(PLOT_UNIQUE_ID %in% plot_unique_id) |>
+      compute()
+  }
+  
+  
+  tree_info |>
+    collect()
   
 }
 
@@ -50,23 +59,23 @@ get_timeseries <- function(selected_trees,
                            connection = "local",
                            local_dir = here::here("data", "arrow")) {
   if(connection == "local") {
-    cn_sources = list.files(here::here(local_dir, "TREE_CN_JOIN"), recursive = T, full.names = T)
+  #  cn_sources = list.files(here::here(local_dir, "TREE_CN_JOIN"), recursive = T, full.names = T)
     raw_sources = list.files(here::here(local_dir, "TREE_RAW"), recursive = T, full.names = T)
-    cn_hive = T
+  #  cn_hive = T
   } else {
-    cn_sources = "https://github.com/diazrenata/in-the-trees/raw/demo/static_data/processed_tables/mn_cns.csv"
+   # cn_sources = "https://github.com/diazrenata/in-the-trees/raw/demo/static_data/processed_tables/mn_cns.csv"
     
     counties <- c("1", "101", "103", "107", "109", "11", "111", "113", "115", "119", "121", "123", "125", "127", "129", "13", "131", "135", "137", "139", "141", "143", "145", "147", "149", "15", "151", "153", "155", "157", "159", "161", "163", "165", "167", "169", "17", "171", "173", "19", "21", "23", "25", "27", "29", "3", "31", "33", "35", "37", "39", "41", "43", "45", "47", "49", "5", "51", "53", "55", "57", "59", "61", "63", "65", "67", "69", "7", "71", "73", "75", "77", "79", "81", "83", "85", "87", "89", "9", "91", "93", "95", "97", "99")
     raw_sources <- paste0("https://github.com/diazrenata/in-the-trees/raw/demo/static_data/processed_tables/STATECD=27/COUNTYCD=", 
                          counties, "/part-0.csv")
-    cn_hive = F
+  #  cn_hive = F
   }
   
-
   report_cols <-
     c(
       c(
-        "TREE_FIRST_CN",
+        "TREE_UNIQUE_ID",
+        "PLOT_UNIQUE_ID",
         "CN",
         "INVYR",
         "STATECD",
@@ -79,18 +88,20 @@ get_timeseries <- function(selected_trees,
       variables
     )
   
-  selected_cns <- selected_trees$TREE_FIRST_CN
+  selected_tree_ids <- selected_trees$TREE_UNIQUE_ID
   
-  cns <-
-    duckdbfs::open_dataset(
-      sources = cn_sources,
-      hive_style = cn_hive,
-      format = "csv"
-    ) |>
-    dplyr::filter(TREE_FIRST_CN %in% selected_cns) |>
-    compute()
+  # selected_cns <- selected_trees$TREE_FIRST_CN
+  # 
+  # cns <-
+  #   duckdbfs::open_dataset(
+  #     sources = cn_sources,
+  #     hive_style = cn_hive,
+  #     format = "csv"
+  #   ) |>
+  #   dplyr::filter(TREE_FIRST_CN %in% selected_cns) |>
+  #   compute()
   
-  timeseries <-
+  tree_timeseries <-
     duckdbfs::open_dataset(
       sources = raw_sources,
       hive_style = T,
@@ -98,7 +109,7 @@ get_timeseries <- function(selected_trees,
     ) |>
     dplyr::mutate(STATECD = as.numeric(STATECD),
                   COUNTYCD = as.numeric(COUNTYCD)) |> 
-    inner_join(cns) |>
+    filter(TREE_UNIQUE_ID %in% selected_tree_ids) |>
     select(all_of(report_cols)) |>
     filter(INVYR >= min_year,
            INVYR <= max_year) |>
