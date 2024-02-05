13138f698e97c296b4eab9cde3eb17309c8131ee
Renata Diaz <renatadiaz@arizona.edu>
2024-02-05 09:21:17 -0700

    pull more columns from TREE, PLOT, and CONDITION tables and set up a second layer of nesting to facilitate rapid filtering by state

--- R/store_as_hive.R
+++ R/store_as_hive.R
@@ -5,41 +5,47 @@ raw_hive <-
            tables = c("TREE", "PLOT", "COND")) {
     
     if("TREE" %in% tables) {
-    trees <-
-      read_csv(
-        here::here(rawdat_dir, paste0(state_to_use, "_TREE.csv")),
-        # here would be where to select additional columns from the TREE table used either for QC or for computing variables for the derived tables
-        # here also note that some of the included columns in the TREE table (DAM-related ones for example) confuse arrow's assumed data types
-        # can specify col types with col_types and schema(), but that is labor intensive. For now I am avoiding this problem by retaining only necessary cols.
-        col_select = c(
-          CN,
-          PREV_TRE_CN,
-          PLT_CN,
-          INVYR,
-          STATECD,
-          UNITCD,
-          COUNTYCD,
-          SUBP,
-          TREE,
-          PLOT,
-          STATUSCD,
-          DIA,
-          HT,
-          ACTUALHT,
-          SPCD,
-          CYCLE
-        )
-      ) |>
-      filter(INVYR >= 2000) |>
-      mutate(TREE_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, SUBP, TREE, sep = "_"),
-             PLOT_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, sep = "_"))
-    
-    write_dataset(
-      trees,
-      here::here(arrow_dir, "TREE_RAW"),
-      format = "csv",
-      partitioning = c("STATECD", "COUNTYCD")
-    )
+      trees <-
+        read_csv(
+          here::here(rawdat_dir, paste0(state_to_use, "_TREE.csv")),
+          # here would be where to select additional columns from the TREE table used either for QC or for computing variables for the derived tables
+          # here also note that some of the included columns in the TREE table (DAM-related ones for example) confuse arrow's assumed data types
+          # can specify col types with col_types and schema(), but that is labor intensive. For now I am avoiding this problem by retaining only necessary cols.
+          col_select = c(
+            CN,
+            PREV_TRE_CN,
+            PLT_CN,
+            INVYR,
+            STATECD,
+            UNITCD,
+            COUNTYCD,
+            SUBP,
+            TREE,
+            PLOT,
+            STATUSCD,
+            DIA,
+            HT,
+            ACTUALHT,
+            SPCD,
+            CYCLE,
+            CONDID,
+            DIAHTCD,
+            HTCD,
+            MORTYR, 
+            MORTCD,
+            SUBCYCLE
+          ),           col_types = cols(.default = "d")
+        ) |>
+        filter(INVYR >= 2000) |>
+        mutate(TREE_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, SUBP, TREE, sep = "_"),
+               PLOT_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, sep = "_"))
+      
+      write_dataset(
+        trees,
+        here::here(arrow_dir, "TREE_RAW", state_to_use),
+        format = "csv",
+        partitioning = c("STATECD", "COUNTYCD")
+      )
     }
     
     if("PLOT" %in% tables) {
@@ -47,35 +53,38 @@ raw_hive <-
       plots <-
         read_csv(
           here::here(rawdat_dir, paste0(state_to_use, "_PLOT.csv")),
-         col_select = c(
-           CN,
-           PREV_PLT_CN,
-           INVYR,
-           STATECD,
-           UNITCD,
-           COUNTYCD,
-           PLOT,
-           PLOT_STATUS_CD,
-           PLOT_NONSAMPLE_REASN_CD,
-           MEASYEAR,
-           MEASMON,
-           MEASDAY,
-           REMPER,
-           KINDCD,
-           DESIGNCD,
-           RDDISTCD,
-           WATERCD,
-           LAT,
-           LON,
-           ELEV
-         )
+          col_select = c(
+            CN,
+            PREV_PLT_CN,
+            INVYR,
+            STATECD,
+            UNITCD,
+            COUNTYCD,
+            PLOT,
+            PLOT_STATUS_CD,
+            PLOT_NONSAMPLE_REASN_CD,
+            MEASYEAR,
+            MEASMON,
+            MEASDAY,
+            REMPER,
+            KINDCD,
+            DESIGNCD,
+            RDDISTCD,
+            WATERCD,
+            LAT,
+            LON,
+            ELEV,
+            CYCLE,
+            SUBCYCLE
+          ),
+          col_types = cols(.default = "d")
         ) |>
         filter(INVYR >= 2000) |>
         mutate(PLOT_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, sep = "_"))
       
       write_dataset(
         plots,
-        here::here(arrow_dir, "PLOT_RAW"),
+        here::here(arrow_dir, "PLOT_RAW", state_to_use),
         format = "csv",
         partitioning = c("STATECD", "COUNTYCD")
       )
@@ -94,18 +103,42 @@ raw_hive <-
             UNITCD,
             COUNTYCD,
             PLOT,
-           CONDID,
-           COND_STATUS_CD,
-           COND_NONSAMPLE_REASN_CD,
-           OWNCD
-          )
+            CONDID,
+            COND_STATUS_CD,
+            COND_NONSAMPLE_REASN_CD,
+            OWNCD,
+            ADFORCD, 
+            BALIVE, 
+            SICOND, 
+            SISP, 
+            SIBASE, 
+            PROP_BASIS,
+            CONDPROP_UNADJ,
+            MICRPROP_UNADJ,
+            SUBPPROP_UNADJ,
+            MACRPROP_UNADJ,
+            SLOPE,
+            ASPECT,
+            DSTRBCD1,
+            DSTRBYR1,
+            DSTRBCD2,
+            DSTRBYR2,
+            DSTRBCD3,
+            DSTRBYR3,
+            CYCLE,
+            SUBCYCLE,
+            SDI_RMRS,
+            SDIMAX_RMRS,
+            SDIPCT_RMRS
+          ),
+          col_types = cols(.default = "d", PROP_BASIS = "c")
         ) |>
         filter(INVYR >= 2000) |>
         mutate(PLOT_UNIQUE_ID = paste(STATECD, UNITCD, COUNTYCD, PLOT, sep = "_"))
       
       write_dataset(
         cond,
-        here::here(arrow_dir, "COND_RAW"),
+        here::here(arrow_dir, "COND_RAW", state_to_use),
         format = "csv",
         partitioning = c("STATECD", "COUNTYCD")
       )
